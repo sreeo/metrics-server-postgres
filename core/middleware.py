bbbb5a10d5df1cde5efd3f43e0ebd80a46e210f2
Sreeraj Rajan <chelseabluerockers@gmail.com>
2023-07-09 02:18:26 -0700

    fix typo

--- core/middleware.py
+++ core/middleware.py
@@ -16,5 +16,5 @@ class LoggingMiddleware:
 
         # Code to be executed for each request/response after
         # the view is called.
-        logger.info(responsed)
+        logger.info(response)
         return response
