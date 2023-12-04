9a8eadd10d7e5a2922c3e51de5abdbc2290b70d7
Sreeraj Rajan <rajan.sreeraj@yahoo.com>
2023-12-04 08:05:09 +0000

    comment out sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -172,7 +172,7 @@ LOGGING = {
 
 # Sentry
 # ------------------------------------------------------------------------------
-SENTRY_DSN = env("SENTRY_DSN", None)
+# SENTRY_DSN = env("SENTRY_DSN", None)
 SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
 
 sentry_logging = LoggingIntegration(
